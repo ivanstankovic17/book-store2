19e2dec34bcc49cee75a6dc9349616e291251f83
Ivan Stankovic <ivan.stankovic@pmf.edu.rs>
2026-04-25 12:58:05 +0200

    Kreran fajl za rad sa bazom

---
+++ database/bookdb.sql
@@ -0,0 +1,8 @@
+CREATE TABLE book (
+                      id int(11) NOT NULL AUTO_INCREMENT,
+                      isbn varchar(30) NOT NULL,
+                      title varchar(255) NOT NULL,
+                      author varchar(255) NOT NULL,
+                      date_added timestamp NOT NULL DEFAULT current_timestamp(),
+                      PRIMARY KEY(id)
+);
